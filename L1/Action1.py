eb16d63ac8f76b25ff3d39c7378db32e3cd0b9c3
heyaug <15091629479@163.com>
2020-01-18 12:41:12 +0800

    提交action2

--- L1/Action1.py
+++ L1/Action1.py
@@ -8,8 +8,6 @@ data = np.load('mnist.npz')
 
 # 分割数据，将25%的数据作为测试集，其余作为训练集
 train_x, test_x, train_y, test_y = train_test_split(data['x_train'], data['y_train'], test_size=0.25, random_state=1)
-
-# 降维
 train_x = train_x.reshape(train_x.shape[0], 28 * 28)
 test_x = test_x.reshape(test_x.shape[0], 28 * 28)
 
@@ -20,5 +18,3 @@ clf = clf.fit(train_x, train_y)
 # 测试
 predict = clf.predict(test_x)
 print(f"cart准确率:{accuracy_score(predict, test_y):.4f}")
-
-# 86.5
